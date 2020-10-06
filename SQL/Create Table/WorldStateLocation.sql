db04e50efc8e016cddb9b8a68bc47d43c2aea3d6
zlin5 <lllreaderlll@gmail.com>
2020-10-06 16:47:26 -0400

    Create WorldStateLocation Tble

---
+++ SQL/Create Table/WorldStateLocation.sql
@@ -0,0 +1,10 @@
+create table innodb.WorldStateLocation( 
+Country_Region varchar(50),
+Province_State varchar(50),
+PRIMARY KEY(Country_Region, Province_State));
+
+select distinct Country_Region,Province_State FROM innodb.tested_worldwide;
+
+insert into innodb.WorldStateLocation
+select distinct Country_Region,Province_State FROM innodb.tested_worldwide;
+
