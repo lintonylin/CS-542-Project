1ace0268024906d8df63148e132ad2817a89afb2
zlin5 <lllreaderlll@gmail.com>
2020-10-06 17:05:16 -0400

    create two tables

--- SQL/Create Table/WorldStateLocation.sql
+++ SQL/Create Table/WorldStateLocation.sql
@@ -3,8 +3,5 @@ Country_Region varchar(50),
 Province_State varchar(50),
 PRIMARY KEY(Country_Region, Province_State));
 
-select distinct Country_Region,Province_State FROM innodb.tested_worldwide;
-
 insert into innodb.WorldStateLocation
 select distinct Country_Region,Province_State FROM innodb.tested_worldwide;
-
---
+++ SQL/Create Table/us_county_location.sql
@@ -0,0 +1,8 @@
+create table innodb.UsCountyLocation ( 
+Country_Region varchar(50),
+State varchar(50),
+Us_county varchar(50),
+PRIMARY KEY(Country_Region, State, Us_county));
+
+insert into innodb.UsCountyLocation
+select distinct "us_county" as Country_Region,state,county from innodb.US_COUNTY_TEST as temp1;
