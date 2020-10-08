b83f67c27199a3ec522d69e4cc762f5d91c69712
zlin5 <lllreaderlll@gmail.com>
2020-10-07 21:41:51 -0400

    NewCasePercent

---
+++ SQL/Query Table/NewCasePercent.sql
@@ -0,0 +1,32 @@
+Select Country_Region, sum(daily_positive) As weekly_positive
+from innodb.WorldTesting
+where dat > '2020-03-07' and dat < '2020-03-14' 
+group by Country_Region;
+
+
+Select sum(weekly_positive) As weekly_sum
+from (
+	Select Country_Region, sum(daily_positive) As weekly_positive
+	from innodb.WorldTesting
+	where dat > '2020-03-07' and dat < '2020-03-14' 
+	group by Country_Region
+) As temp;
+
+
+SELECT
+a.Country_Region,
+CONCAT( ROUND( a.weekly_positive / b.weekly_sum * 100, 2 ), '', '%' ) AS percent 
+FROM
+( Select Country_Region, sum(daily_positive) As weekly_positive
+from innodb.WorldTesting
+where dat > '2020-03-07' and dat < '2020-03-14' 
+group by Country_Region ) a,
+(
+Select sum(weekly_positive) As weekly_sum
+from (
+	Select Country_Region, sum(daily_positive) As weekly_positive
+	from innodb.WorldTesting
+	where dat > '2020-03-07' and dat < '2020-03-14' 
+	group by Country_Region
+) As temp
+) b
