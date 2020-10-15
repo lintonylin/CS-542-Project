98c16c07d3061f78adfcc98d5d48a4663893648b
WTZ219 <72417952+WTZ219@users.noreply.github.com>
2020-10-15 14:14:12 -0400

    Update NewDeathPercent.py

--- FlaskBackend/Query/NewDeathPercent.py
+++ FlaskBackend/Query/NewDeathPercent.py
@@ -1,15 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Oct 13 17:41:21 2020
-
-@author: hawks
-"""
-
 #from flask import Flask
 #from flask_sqlalchemy import SQLAlchemy
 import pymysql
-import json
-from datetime import datetime
 
 host = 'cs542.chpy8wzjpky3.us-east-1.rds.amazonaws.com'
 user = 'admin'
@@ -18,40 +9,13 @@ port=3306
 conn = pymysql.connect(host, user=user, port=port, passwd=password)
 c = conn.cursor()
 
-#Validate input
-def validate(date_text):
-    try:
-         datetime.strptime(startdate, "%Y-%m-d")
-    except ValueError:
-          print("This is the incorrectdate format. It should be YYYY-MM-DD")
+startdate = '\'2020-03-07\'' # input json
+enddate = '\'2020-03-14\'' # input json
 
-#Get user input
-startdate = input("What is the startdate(format: yyyy-mm-dd): ")
+query = "SELECT a.Country_Region, CONCAT( ROUND( a.weekly_death / b.weekly_sum * 100, 2 ), \'\', \'%\' ) AS percent FROM ( Select Country_Region, sum(death) AS weekly_death from innodb.world_death Where dat > {0} and dat < {1} group by Country_Region) a,(Select sum(weekly_death) As weekly_sum from (Select Country_Region, sum(death) AS weekly_death from innodb.world_death Where dat > {2} and dat < {3} group by Country_Region) AS temp) b;".format(startdate, enddate, startdate, enddate)
 
-validate(startdate)
 
-enddate = input("What is the enddate(format: yyyy-mm-dd): ")
-
-validate(enddate)
-
-#Execute query
-c.execute("SELECT a.Country_Region, CONCAT( ROUND( a.weekly_death / b.weekly_sum * 100, 2 ), '', '%' ) AS percent FROM( Select Country_Region, sum(death) AS weekly_death from innodb.world_death Where dat > '"+ startdate+"' and dat < '"+ enddate +"' group by Country_Region) a,(Select sum(weekly_death) As weekly_sum from (Select Country_Region, sum(death) AS weekly_death from innodb.world_death Where dat > '"+ startdate +"' and dat < '"+ enddate +"' group by Country_Region) AS temp) b;")
+c.execute(query)
 
 result = c.fetchall()
-
-#Validate result 
-if len(result) == 0:
-    print("Please try it again with correct date format")
-else: 
-    print(result)
-
-#Write json
-Death_result = []
-
-for i in range(len(result)):
-    country = result[i][0]
-    Death_result = Death_result + [[country, result[i][1]]]
-
-with open ("NewDeathPercentbyCountry.json","w") as f:
-    json.dump(Death_result, f)
-   
+print(result)
