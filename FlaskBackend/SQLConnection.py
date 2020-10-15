dcfd1b085f5351ecc1bcb3694554fab4c16e55e5
zlin5 <lllreaderlll@gmail.com>
2020-10-15 12:55:26 -0400

    new query based on python

--- FlaskBackend/SQLConnection.py
+++ FlaskBackend/SQLConnection.py
@@ -9,7 +9,12 @@ port=3306
 conn = pymysql.connect(host, user=user, port=port, passwd=password)
 c = conn.cursor()
 
+startdate = '' # input json
+enddate = '' # input json
+
 c.execute('Select Country_Region, sum(daily_positive) As weekly_positive from innodb.WorldTesting where dat > \'2020-03-07\' and dat < \'2020-03-14\' group by Country_Region;')
 
 result = c.fetchall()
 print(result)
+
+# store result in json
