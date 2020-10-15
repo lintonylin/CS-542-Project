08df380021834b77fcdb63eefdba42427df5483a
zlin5 <lllreaderlll@gmail.com>
2020-10-15 16:30:26 -0400

    InputOutput

--- FlaskBackend/NewCaseCountryPercent.py
+++ FlaskBackend/NewCaseCountryPercent.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 import pymysql
+import json
 
 host = 'cs542.chpy8wzjpky3.us-east-1.rds.amazonaws.com'
 user = 'admin'
@@ -9,8 +10,12 @@ port=3306
 conn = pymysql.connect(host, user=user, port=port, passwd=password)
 c = conn.cursor()
 
-startdate = '\'2020-03-07\'' # input json
-enddate = '\'2020-03-14\'' # input json
+inputjson = "{\"startdate\": \"2020-03-07\", \"enddate\": \"2020-03-14\"}"
+
+inputjson = json.loads(inputjson)
+
+startdate = "\"" + inputjson['startdate'] + "\"" # input json
+enddate = "\"" + inputjson['enddate'] + "\"" # input json
 
 query = "SELECT a.Country_Region, a.weekly_positive, CONCAT( ROUND( a.weekly_positive / b.weekly_sum * 100, 2 ), \'\', \'%\' ) AS percent FROM( Select Country_Region, sum(daily_positive) As weekly_positive from innodb.WorldTesting where dat > {0} and dat < {1} group by Country_Region ) a,(Select sum(weekly_positive) As weekly_sum from (Select Country_Region, sum(daily_positive) As weekly_positive from innodb.WorldTesting where dat > {2} and dat < {3} group by Country_Region) As temp) b; ".format(startdate, enddate, startdate, enddate)
 
@@ -18,6 +23,12 @@ query = "SELECT a.Country_Region, a.weekly_positive, CONCAT( ROUND( a.weekly_pos
 c.execute(query)
 
 result = c.fetchall()
-print(result)
 
 # store result in json
+outputjson = {}
+for r in result:
+    outputjson[r[0]] = []
+    outputjson[r[0]].append(int(r[1]))
+    outputjson[r[0]].append(r[2])
+
+print(json.dumps(outputjson))
