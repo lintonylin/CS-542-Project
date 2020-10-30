562095d26c812d323de284c638ffedb1be9b19cc
WTZ219 <72417952+WTZ219@users.noreply.github.com>
2020-10-30 19:22:19 -0400

    Update NewDeathPercent.py

--- FlaskBackend/Query/NewDeathPercent.py
+++ FlaskBackend/Query/NewDeathPercent.py
@@ -19,7 +19,7 @@ inputjson = json.loads(inputjson)
 startdate = "\"" + inputjson['startdate']+ "\""
 enddate = "\"" +inputjson['enddate']+"\""
 
-query = "SELECT a.Country_Region, a.weekly_death, CONCAT( ROUND( a.weekly_death / b.weekly_sum * 100, 2 ), \'\', \'%\' ) AS percent FROM ( Select Country_Region, sum(death) AS weekly_death from innodb.world_death Where dat > {0} and dat < {1} group by Country_Region) a,(Select sum(weekly_death) As weekly_sum from (Select Country_Region, sum(death) AS weekly_death from innodb.world_death Where dat > {2} and dat < {3} group by Country_Region) AS temp) b;".format(startdate, enddate, startdate, enddate)
+query = "SELECT a.Country_Region, a.weekly_death, CONCAT( ROUND( a.weekly_death / b.weekly_sum * 100, 2 ), \'\', \'%\' ) AS percent FROM ( Select Country_Region, sum(death) AS weekly_death from innodb.world_death Where dat > {0} and dat < {1} group by Country_Region) a,(Select sum(weekly_death) As weekly_sum from (Select Country_Region, sum(death) AS weekly_death from innodb.world_death Where dat > {2} and dat < {3} group by Country_Region) AS temp) b order by a.weekly_death desc;".format(startdate, enddate, startdate, enddate)
 
 
 c.execute(query)
