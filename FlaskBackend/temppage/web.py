f95d086721a8564ec2d33d9cb0a16d703d7ecf28
zlin5 <lllreaderlll@gmail.com>
2020-11-10 19:06:54 -0500

    Change search 4

--- FlaskBackend/temppage/web.py
+++ FlaskBackend/temppage/web.py
@@ -63,7 +63,7 @@ def search4():
     if request.values.get('country') == '':
         startdate = "\"" + request.values.get('startdate') + "\""
         enddate = "\"" + request.values.get('enddate') + "\""
-        query = "SELECT a.Country_Region, a.weekly_positive as positive, CONCAT( ROUND( a.weekly_positive / b.weekly_sum * 100, 2 ), \'\', \'%\' ) AS percent FROM( Select Country_Region, sum(daily_positive) As weekly_positive from innodb.WorldTesting where dat > {0} and dat < {1} group by Country_Region ) a,(Select sum(weekly_positive) As weekly_sum from (Select Country_Region, sum(daily_positive) As weekly_positive from innodb.WorldTesting where dat > {2} and dat < {3} group by Country_Region) As temp) b order by a.weekly_positive desc; ".format(
+        query = "SELECT a.Country_Region, a.weekly_positive as positive, CONCAT( ROUND( a.weekly_positive / b.weekly_sum * 100, 2 ), \'\', \'%\' ) AS percent FROM( Select Country_Region, sum(daily_positive) As weekly_positive from innodb.WorldTesting where dat > {0} and dat < {1} and Province_State='All States' group by Country_Region ) a,(Select sum(weekly_positive) As weekly_sum from (Select Country_Region, sum(daily_positive) As weekly_positive from innodb.WorldTesting where dat > {2} and dat < {3} and Province_State='All States' group by Country_Region) As temp) b order by a.weekly_positive desc; ".format(
             startdate, enddate, startdate, enddate)
         cur.execute(query)
         datas = cur.fetchall()
@@ -77,7 +77,7 @@ def search4():
         startdate = "\"" + request.values.get('startdate') + "\""
         enddate = "\"" + request.values.get('enddate') + "\""
         country = "\"" + request.values.get('country') + "\""
-        query = "SELECT a.Country_Region, a.weekly_positive as positive, CONCAT( ROUND( a.weekly_positive / b.weekly_sum * 100, 2 ), \'\', \'%\' ) AS percent FROM( Select Country_Region, sum(daily_positive) As weekly_positive from innodb.WorldTesting where dat > {0} and dat < {1} group by Country_Region ) a,(Select sum(weekly_positive) As weekly_sum from (Select Country_Region, sum(daily_positive) As weekly_positive from innodb.WorldTesting where dat > {2} and dat < {3} group by Country_Region) As temp) b where a.Country_Region={4}; ".format(startdate, enddate, startdate, enddate, country)
+        query = "SELECT a.Country_Region, a.weekly_positive as positive, CONCAT( ROUND( a.weekly_positive / b.weekly_sum * 100, 2 ), \'\', \'%\' ) AS percent FROM( Select Country_Region, sum(daily_positive) As weekly_positive from innodb.WorldTesting where dat > {0} and dat < {1} and Province_State='All States' group by Country_Region ) a,(Select sum(weekly_positive) As weekly_sum from (Select Country_Region, sum(daily_positive) As weekly_positive from innodb.WorldTesting where dat > {2} and dat < {3} and Province_State='All States' group by Country_Region) As temp) b where a.Country_Region={4}; ".format(startdate, enddate, startdate, enddate, country)
         cur.execute(query)
         datas = cur.fetchall()
         return render_template('search4.html',items=datas)
--- SQL/Query Table/NewCasePercent.sql
+++ SQL/Query Table/NewCasePercent.sql
@@ -20,14 +20,15 @@ CONCAT( ROUND( a.weekly_positive / b.weekly_sum * 100, 2 ), '', '%' ) AS percent
 FROM
 ( Select Country_Region, sum(daily_positive) As weekly_positive
 from innodb.WorldTesting
-where dat > '2020-03-07' and dat < '2020-03-14' 
+where dat > '2020-03-07' and dat < '2020-03-14' and Province_State='All States'
 group by Country_Region ) a,
 (
 Select sum(weekly_positive) As weekly_sum
 from (
 	Select Country_Region, sum(daily_positive) As weekly_positive
 	from innodb.WorldTesting
-	where dat > '2020-03-07' and dat < '2020-03-14' 
+	where dat > '2020-03-07' and dat < '2020-03-14' and Province_State='All States'
 	group by Country_Region
 ) As temp
-) b; 
+) b
+where a.Country_Region='Canada'; 
