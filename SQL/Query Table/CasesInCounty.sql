382a6eef793b9fc64ec676be4dd83350c988aecc
Irelia <yguan2@wpi.edu>
2020-11-11 14:52:35 -0500

    Update CasesInCounty.sql

--- SQL/Query Table/CasesInCounty.sql
+++ SQL/Query Table/CasesInCounty.sql
@@ -1,3 +1,4 @@
 
-select cases from innodb.UsCountyCaseInformation
-where dat = '2020-06-02' and state = 'California' and us_county = 'San Francisco';
+SELECT state, us_county, max(cast(cases as decimal)) - min(cast(cases as decimal)) as result FROM innodb.UsCountyCaseInformation
+where state='Massachusetts' and (dat=date_sub(cast('2020-03-11' as date),interval 1 day) or dat='2020-03-11')
+group by state, us_county;
